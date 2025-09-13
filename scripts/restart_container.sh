15580df8c716d07a83987a8134e8cacc6e1b9072
Madhan14 <madhann3333@gmail.com>
2025-09-13 23:21:51 +0530

    5

--- scripts/restart_container.sh
+++ scripts/restart_container.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-docker stop simple-docker-service || true
-docker rm simple-docker-service || true
-docker run -d -p 80:80 --name simple-docker-service 333650976464.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service:latest
+set -e
+
+# Stop old container if running
+docker stop simple-docker || true
+docker rm simple-docker || true
+
+# Pull latest image
+docker pull 333650976464.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service:latest
+
+# Run new container on port 8899
+docker run -d -p 8899:80 --name simple-docker 333650976464.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service:latest
