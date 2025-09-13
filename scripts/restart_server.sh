4870a921736604d5277c826e06a7dc720e12ab22
Madhan <madhann3333@gmail.com>
2025-09-13 18:26:09 +0530

    Update restart_server.sh

--- scripts/restart_server.sh
+++ scripts/restart_server.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-sudo systemctl restart httpd
+docker stop simple-docker-service || true
+docker rm simple-docker-service || true
+docker run -d -p 80:80 --name simple-docker-service 333650976464.dkr.ecr.ap-south-1.amazonaws.com/simple-docker-service:latest
